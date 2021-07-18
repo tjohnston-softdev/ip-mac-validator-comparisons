7e8232a6cdebf48cf356fd4421c67dec474938f3
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-07-18 12:57:38 +1000

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,11 @@
+const mocha = require("mocha");
+const chai = require("chai");
+const expect = chai.expect;
+
+describe("Description", function()
+{
+	it("Test Case", function()
+	{
+		expect(true).to.be.true;
+	});
+});
